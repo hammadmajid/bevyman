1d3e043fd1d7cd0009693178b128c0655db20633
Hammad Majid <hm.hammadmajid@gmail.com>
2023-07-19 18:55:56 +0500

    impl example from Bevy the book
    create new app with default plugins
    insert time resource that will say hello to people every two seconds
    add a Startup system add_people
    add a Update system say_hello

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .add_systems(Startup, add_people)
+        .add_systems(Update, say_hello)
+        .run();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn say_hello(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}, from BevyEngine!", name.0);
+        }
+    }
 }
