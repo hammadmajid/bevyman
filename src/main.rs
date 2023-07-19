10bbcceac55734e5065230f3810737bd17241af5
Hammad Majid <hm.hammadmajid@gmail.com>
2023-07-19 23:25:49 +0500

    Revert "remove Bevy book example and load static start_screen.png"
    This reverts commit e72280c0950445188e5069efd10f068d04e1d27c.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,31 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .add_systems(Startup, add_people)
+        .add_systems(Update, say_hello)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("start_screen.png"),
-        ..default()
-    });
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn say_hello(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}, from BevyEngine!", name.0);
+        }
+    }
 }
